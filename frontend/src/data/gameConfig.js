146ce3f2c02682e2015ea9eff99c4b24c4731f1a
nitsesports <esports.nits@gmail.com>
2025-11-07 22:18:15 +0530

    Update gameConfig.js
    testing of payment gateway

--- frontend/src/data/gameConfig.js
+++ frontend/src/data/gameConfig.js
@@ -3,31 +3,31 @@ export const gameConfig = {
     id: "bgmi",
     name: "BGMI",
     playerCount: 4,
-    price: { nits: 100, other: 250 },
+    price: { nits: 1, other: 2 },
   },
   valorant: {
     id: "valorant",
     name: "Valorant",
     playerCount: 5,
-    price: { nits: 100, other: 250 },
+    price: { nits: 1, other: 2 },
   },
   freefire: {
     id: "freefire",
     name: "Free Fire",
     playerCount: 4,
-    price: { nits: 100, other: 250 },
+    price: { nits: 1, other: 2 },
   },
   codm: {
     id: "codm",
     name: "COD Mobile",
     playerCount: 5,
-    price: { nits: 100, other: 250 },
+    price: { nits: 1, other: 2 },
   },
   ml: {
     id: "ml",
     name: "Mobile Legends",
     playerCount: 5,
-    price: { nits: 100, other: 250 },
+    price: { nits: 1, other: 2 },
   },
   fifa: {
     id: "fifa",
@@ -39,13 +39,13 @@ export const gameConfig = {
     id: "bulletchoe",
     name: "Bullet Echo",
     playerCount: 3,
-    price: { nits: 100, other: 250 },
+    price: { nits: 1, other: 2 },
   },
   clashroyale: {
     id: "clashroyale",
     name: "Clash Royale",
     playerCount: 1,
-    price: { nits: 100, other: 250 },
+    price: { nits: 1, other: 2 },
   },
   
 };
